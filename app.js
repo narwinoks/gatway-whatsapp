1861aec2286aafb532c03bb962098e243ea228f1
narwinoks <narnowin195@gmail.com>
2022-12-27 13:12:40 +0700

    updated

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const bodyParser = require("body-parser");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const mime = require("mime-types");
+const https = require("https");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -29,6 +30,26 @@ app.get("/", (req, res) => {
   res.sendFile("index.html", {
     root: __dirname,
   });
+});
+
+app.get("/testing", async (req, res) => {
+  let data = "";
+  axios
+    .get("https://api-berita-indonesia.vercel.app/antara/lifestyle/", {
+      headers: { "Accept-Encoding": "gzip,deflate,compress" },
+    })
+    .then((ping) => {
+      const news = ping.data.data.posts[0];
+      data = ` 
+      ========= Berita Hari Ini ========
+          1 . title : ${news.title}
+          2 . link : ${news.link}
+      `;
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log("Error: ", err.message);
+    });
 });
 
 const client = new Client({
@@ -50,17 +71,17 @@ const client = new Client({
 });
 
 // SEND MESSAGE FOR CLIENT
-client.on("message", (message) => {
-  if (msg.body == "!ping") {
-    msg.reply("pong");
-  } else if (msg.body == "good morning") {
-    msg.reply("hai");
-  } else if (msg.body == "!groups") {
+client.on("message", async (message) => {
+  if (message.body == "!ping") {
+    message.reply("pong");
+  } else if (message.body == "good morning") {
+    message.reply("hai");
+  } else if (message.body == "!groups") {
     client.getChats().then((chats) => {
       const groups = chats.filter((chat) => chat.isGroup);
 
       if (groups.length == 0) {
-        msg.reply("You have no group yet.");
+        message.reply("You have no group yet.");
       } else {
         let replyMsg = "*YOUR GROUPS*\n\n";
         groups.forEach((group, i) => {
@@ -68,11 +89,151 @@ client.on("message", (message) => {
         });
         replyMsg +=
           "_You can use the group id to send a message to the group._";
-        msg.reply(replyMsg);
+        message.reply(replyMsg);
       }
     });
-  } else if (msg.body === "!test") {
+  } else if (message.body === "!test") {
     client.sendMessage(message.from, "test");
+  } else if (message.body === "!lis-command") {
+    message.reply(
+      `
+==== DAFTAR PERINTAH BOT ====
+!k-announcement
+      
+!k-info
+      
+!k-dev
+      
+!k-anggota
+
+!k-news
+          
+!k-owner
+
+!groups(jangan terlalu sering)
+
+
+=== PT .Berkah Jaya Nusantara Wwkkw =`
+    );
+
+  } else if (message.body === "!k-info") {
+    client.sendMessage(message.from, "ini info");
+  } else if (message.body === "!k-announcement") {
+    const msgx = `
+    [REMINDER]
+
+Please, ini cuma pengumuman biasa aja ﻿ 
+    
+Terms and Conditions 
+    
+Please send this message﻿
+================
+!k-announcement﻿ ﻿﻿﻿ 
+================
+    
+Alert!!! ﻿ 
+    
+Thank you for your attention`;
+    client.sendMessage(message.from, msgx);
+  } else if (message.body === "!k-news") {
+    let data = "";
+    axios
+      .get("https://api-berita-indonesia.vercel.app/antara/lifestyle/", {
+        headers: { "Accept-Encoding": "gzip,deflate,compress" },
+      })
+      .then((ping) => {
+        const news = ping.data.data.posts[0];
+        data = ` 
+========= Berita Hari Ini ========
+1 . title : ${news.title}
+2 . link : ${news.link}
+        `;
+        client.sendMessage(message.from, data);
+      })
+      .catch((err) => {
+        console.log("Error: ", err.message);
+      });
+  } else if (message.body === "!k-anggota") {
+    client.sendMessage(
+      message.from,
+      `
+========= Daftar Anggota ========
+1.Dwi Wijayanto (001)
+dwin@gamil.com
+  [programmer]
+  @gmail.com
+2.Winarno (002)
+  narnowin195@gmail.com
+  [Laravel Programmer at  cyberolympus]
+  @gmail.com
+3.Ridlo Fauzi (003)
+  [Student at the University of Duta Bangsa University]
+  @gmail.com
+4.Satria Baja Hitam (004)
+  [Student at the University of Muhammadiyah University Of Karayanganyar]
+  @gmail.com
+5.JEFA (005)
+  [Student at the University of Muhammadiyah University Of Karayanganyar]
+  @gmail.com
+6.Rian Maulana (006)
+  [Laravel Programmer At Yureka Teknologi Cipta]
+  @gmail.com  
+    `
+    );
+  } else if (message.body === "!k-dev") {
+    const msgz = `
+Daftar Channel Youtube Programming & Teknologi Indonesia indonesia
+1.BackEnd
+  a.Kelas Terbuka           
+  (https://www.youtube.com/c/KelasTerbuka) Python, Java, C++, Matlab, Livestreaming
+
+  b.Programmer Zaman Now    
+  (https://www.youtube.com/c/ProgrammerZamanNow) Python, Docker, Elasticsearch, Kubernetes, TypeScript, JS,
+  Redis, MongoDB, Java, PHP, MySQL	
+
+  c.Kawan Koding	        
+  (https://www.youtube.com/c/kawankoding) Laravel, Livewire	
+
+  d.Parsinta	              
+  (https://www.youtube.com/c/Parsinta) Laravel, TDD, Breeze, Vue, Tailwind	
+
+  e. VIP CODE STUDIO		      
+  https://www.youtube.com/c/vipcodestudio) NodeJS, Discord.js, Figma	
+      
+  f.Agung Setiawan	        
+  (https://www.youtube.com/c/AgungSetiawanCoding) Golang, Python, Ruby	
+    
+2.Frontend
+  a.ArrayID                 
+    (https://www.youtube.com/c/KelasTerbuka) JS, CSS, TailwindCSS, NextJS, ReactJS
+
+  b.Balademy                
+    (https://www.youtube.com/channel/UCo2MhO0TrEUKdL9Pt-JNgLg) Angular, Livewire, Vue	
+
+  c.Prawito Hudoro	        
+    (https://www.youtube.com/c/prawitohudoro) ReactJS, NextJS, React Native, MERN, JS, Livestreaming	
+
+  d.Wahidev Academy		      
+    (https://www.youtube.com/c/WahidevAcademy) React, React Native 
+
+      
+3.General
+  a.Wpu                      
+    (http://youtube.com/webprogrammingunpas)HTML, CSS, Javascript, PHP, Git, GitHub, SASS,
+    Bootstrap, NodeJS, CodeIgniter, Laravel, Livestreaming
+  
+    `;
+    client.sendMessage(message.from, msgz);
+  } else if (message.body == "!k-owner") {
+    if (message.hasMedia) {
+      const media = await MessageMedia.fromUrl(
+        "https://via.placeholder.com/350x150.png"
+      );
+      message.sendMessage(media, "saya owner");
+      // do something with the media data here
+    } else {
+      message.reply("hai");
+    }
   }
 });
 client.initialize();
