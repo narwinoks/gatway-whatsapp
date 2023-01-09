f0580a583bfc6b57ca9fe2543dbfd8b576c19070
narwinoks <narnowin195@gmail.com>
2023-01-09 09:43:47 +0700

    updated

--- app.js
+++ app.js
@@ -32,24 +32,6 @@ app.get("/", (req, res) => {
   });
 });
 
-// const client = new Client({
-//   restartOnAuthFail: true,
-//   puppeteer: {
-//     headless: true,
-//     args: [
-//       "--no-sandbox",
-//       "--disable-setuid-sandbox",
-//       "--disable-dev-shm-usage",
-//       "--disable-accelerated-2d-canvas",
-//       "--no-first-run",
-//       "--no-zygote",
-//       "--single-process", // <- this one doesn't works in Windows
-//       "--disable-gpu",
-//     ],
-//   },
-//   authStrategy: new LocalAuth(),
-// });
-
 const client = new Client();
 
 client.on("ready", function () {
